2fe0d13c31b60436b4f47f2e8cbd62d59be7d9bc
David <david.concejero.hernandez@gmail.com>
2020-03-03 18:20:26 +0100

    php documentor

---
+++ js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Calcula",
+            "name": "Calcula",
+            "summary": "",
+            "url": "../classes/Calcula.html"
+        },                {
+            "fqsen": "\\Calcula\u003A\u003Asuma\u0028\u0029",
+            "name": "suma",
+            "summary": "suma\u0020dos\u0020numeros",
+            "url": "../classes/Calcula.html#method_suma"
+        },                {
+            "fqsen": "\\Calcula\u003A\u003Aresta\u0028\u0029",
+            "name": "resta",
+            "summary": "resta\u0020dos\u0020numeros",
+            "url": "../classes/Calcula.html#method_resta"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "../namespaces/default.html"
+        }            ]
+);
